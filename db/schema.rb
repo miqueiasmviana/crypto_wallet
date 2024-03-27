36cb2986a3724468ab39877ecb8db67e2c75734d
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-03-27 12:13:14 -0300

    aula 70

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_27_134346) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_27_150646) do
   create_table "coins", force: :cascade do |t|
     t.string "description"
     t.string "acronym"
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_27_134346) do
   end
 
   create_table "mining_types", force: :cascade do |t|
-    t.string "name"
+    t.string "description"
     t.string "acronym"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
