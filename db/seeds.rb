558c7623847c6c0da36dbe914919d13f0178c659
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-03-27 11:51:50 -0300

    aula 69

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,31 +8,4 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-spinner = TTY::Spinner.new("[:spinner] Cadastrando Moedas...")
-spinner.auto_spin
-
-coins = [
-  {
-    description: "Bitcoin",
-    acronym: "BTC",
-    url_image: "https://images.ctfassets.net/ohcexbfyr6py/6ncFHBdtEFrtrXZrK6D0CG/8682659d1744caac1ed5d627fc58b779/btc_logo.png"
-  },
-
-  {
-    description: "Ethereum",
-    acronym: "ETH",
-    url_image: "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1696501628"
-  },
-
-  {
-    description: "Dash",
-    acronym: "DASH",
-    url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/131.png"
-  }
-]
-
-coins.each do |coin|
-  Coin.find_or_create_by!(coin)
-end
-
-spinner.success("(Concluído!)")
+#use rails dev:setup(lib/tasks/dev.rake)
