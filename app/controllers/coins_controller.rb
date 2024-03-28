090ba3a015e122b2976bb17b9c2c4a76ea3020f5
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-03-28 10:38:35 -0300

    aula 78 - refatorando os códigos de select para o padrão MVC, tirando da view e colocando no controller

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,5 +1,6 @@
 class CoinsController < ApplicationController
   before_action :set_coin, only: %i[ show edit update destroy ]
+  before_action :set_mining_type_options, only: [:new, :create, :edit, :update]
   layout "adm"
   
   # GET /coins or /coins.json
@@ -59,6 +60,9 @@ class CoinsController < ApplicationController
   end
 
   private
+    def set_mining_type_options
+      @mining_type_options = MiningType.all.pluck(:description, :id)
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params[:id])
